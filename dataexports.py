3ddfdec743492bdccfed12ece34caad26ef3dc1a
Winston <winston.barlow@code.berlin>
2019-04-11 13:26:11 +0200

    exporting transposed table

--- dataexports.py
+++ dataexports.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('global_dataset.csv', delimiter = ",", header=0)
+# df = pd.read_csv('global_dataset.csv', delimiter = ",", header=0)
 
-reduced_data = df.groupby(['yearOfRegistration', 'citizenship']).count()['case_id'].unstack()
-reduced_data.to_csv(r'data\countries.csv')
+# reduced_data = df.groupby(['yearOfRegistration', 'citizenship']).count()['case_id'].unstack()
+df2 = pd.read_csv('data/cases_per_citizenship.csv', delimiter = ",", header=0)
+
+df2.groupby(['yearOfRegistration'])
+
+df2 = df2.T
+
+df2.to_csv(r'transposed.csv')
