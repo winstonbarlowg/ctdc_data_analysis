165df7bdf08ac6f2a81b4d445ecf5158e9be1f6c
Winston <winston.barlow@code.berlin>
2019-04-11 15:25:18 +0200

    added reference comments

--- myapp.py
+++ myapp.py
@@ -18,7 +18,18 @@ def maps():
 def scatter():
     return render_template('scatter.html')
 
+# run app in debug mode, running the script directly to save time
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
+
+
+
+
+
+
+'''
+If the source file is executed as the main program, 
+the interpreter sets the __name__ variable to have a value “__main__”. 
+If this file is being imported from another module, __name__ will be set to the module’s name.
+'''
