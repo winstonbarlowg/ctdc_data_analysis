34ae561b15ce1123a89c7ae22127619a9adfd4e1
Winston <winston.barlow@code.berlin>
2019-04-12 02:21:27 +0200

    minor layout fixes

--- dataexports.py
+++ dataexports.py
@@ -1,9 +1,10 @@
+# use this script to export sub sets of data for better handling
 import pandas as pd
 import json
 
 # df = pd.read_csv('global_dataset.csv', delimiter = ",", header=0)
-
 # reduced_data = df.groupby(['yearOfRegistration', 'citizenship']).count()['case_id'].unstack()
+
 df2 = pd.read_csv('data/cases_per_citizenship.csv', delimiter = ",", header=0)
 
 df2.groupby(['yearOfRegistration'])
