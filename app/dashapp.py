4978cfc47ca8a59953ddc7dcb82626b94601c297
Winston <winston.barlow@code.berlin>
2019-04-17 00:41:36 +0200

    simple dynamic chart visualisation

--- app/dashapp.py
+++ app/dashapp.py
@@ -8,26 +8,27 @@ import pandas as pd
 import time
 import os
 
-server = flask.Flask('app')
-server.secret_key = os.environ.get('secret_key', 'secret')
+# server = flask.Flask('app')
+# server.secret_key = os.environ.get('secret_key', 'secret')
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
+df = pd.read_csv('data/gender_cases_per_year.csv', delimiter = ",", header=0)
 
-app = dash.Dash('app', server=server)
+# app = dash.Dash('app', server=server)
 
 app.scripts.config.serve_locally = False
 dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
 
 app.layout = html.Div([
-    html.H1('Stock Tickers'),
+    html.H1('Human Trafficking Cases per Year by Gender'),
     dcc.Dropdown(
         id='my-dropdown',
         options=[
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'},
-            {'label': 'Coke', 'value': 'COKE'}
+        # replace value for corresponding key
+            {'label': 'Female', 'value': 'Female'},
+            {'label': 'Male', 'value': 'Male'},
+            {'label': 'Unknown', 'value': 'Unknown'}
         ],
-        value='TSLA'
+        value='UA'
     ),
     dcc.Graph(id='my-graph')
 ], className="container")
@@ -36,11 +37,11 @@ app.layout = html.Div([
               [Input('my-dropdown', 'value')])
 
 def update_graph(selected_dropdown_value):
-    dff = df[df['Stock'] == selected_dropdown_value]
+    dff = df[df['Gender'] == selected_dropdown_value]
     return {
         'data': [{
-            'x': dff.Date,
-            'y': dff.Close,
+            'x': dff.year,
+            'y': dff.cases,
             'line': {
                 'width': 3,
                 'shape': 'spline'
