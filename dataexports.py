8e4449fa880369c3d73df28e3858985bdafb479c
Winston <winston.barlow@code.berlin>
2019-04-16 22:44:17 +0200

    fixes

--- dataexports.py
+++ dataexports.py
@@ -2,13 +2,17 @@
 import pandas as pd
 import json
 
-# df = pd.read_csv('global_dataset.csv', delimiter = ",", header=0)
+df = pd.read_csv('global_dataset.csv', delimiter = ",", header=0)
 # reduced_data = df.groupby(['yearOfRegistration', 'citizenship']).count()['case_id'].unstack()
 
-df2 = pd.read_csv('data/cases_per_citizenship.csv', delimiter = ",", header=0)
+# df2 = pd.read_csv('data/cases_per_citizenship.csv', delimiter = ",", header=0)
+#
+# df2.groupby(['yearOfRegistration'])
+#
+# df2 = df2.T
+#
+# df2.to_csv(r'transposed.csv')
 
-df2.groupby(['yearOfRegistration'])
+genders = df.groupby(['yearOfRegistration', 'gender']).count()['case_id'].unstack()
 
-df2 = df2.T
-
-df2.to_csv(r'transposed.csv')
+genders.to_csv('gender_cases_per_year.csv')
