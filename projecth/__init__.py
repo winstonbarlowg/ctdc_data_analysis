e335268606421dd0e13320a1ddebb6e944119715
Winston <winston.barlow@code.berlin>
2019-04-18 19:24:15 +0200

    fixing routes

--- projecth/__init__.py
+++ projecth/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-import dash
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-app = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')
-
-from app.routes import *
-from app.dashapp import *
+from projecth.routes import *
--- run.py
+++
@@ -1,5 +0,0 @@
-import os
-from projecth import *
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
