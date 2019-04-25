5fae69dc4738cbba0145eb8802f6c23f9f114339
Winston <winston.barlow@code.berlin>
2019-04-25 14:35:39 +0200

    export to json

--- resources/tojson.py
+++ resources/tojson.py
@@ -1,15 +1,24 @@
 import csv
 import json
 
-csvFilePath = 'global_dataset.csv'
-jsonFilePath = 'global_dataset.json'
+csvFilePath = 'female_typeOfExploit.csv'
+jsonFilePath = 'female_typeOfExploit.json'
 
-data = {}
-with open(csvFilePath) as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for csvRow in csvReader:
-        hmid = csvRow["case_id"]
-        data[hmid] = csvRow
+# data = {}
+# with open(csvFilePath) as csvFile:
+#     csvReader = csv.DictReader(csvFile)
+#     for csvRow in csvReader:
+#         hmid = csvRow["typeOfExploitConcatenated"]
+#         data[hmid] = csvRow
+#
+# with open(jsonFilePath, "w") as jsonFile:
+#     jsonFile.write(json.dumps(data, indent=4))
 
-with open(jsonFilePath, "w") as jsonFile:
-    jsonFile.write(json.dumps(data, indent=4))
+csvfile = open('female_typeOfExploit.csv', 'r')
+jsonfile = open('female_typeOfExploit.json', 'w')
+
+fieldnames = ("type","number")
+reader = csv.DictReader( csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
